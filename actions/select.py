b19514b9248837f85753ac968e13fe820fbda6d9
Joe Topjian <joe@topjian.net>
2017-01-20 07:06:56 +0000

    Adding key parameter to store results in a datastore key. This is to get around the 'Argument list too long' error

--- actions/select.py
+++ actions/select.py
@@ -1,11 +1,13 @@
 import MySQLdb
 
 from lib.base import MySQLBaseAction
+from st2client.client import Client
+from st2client.models import KeyValuePair
 
 
 class MySQLSelectAction(MySQLBaseAction):
 
-    def run(self, connection, query, data, host, user, passwd, db):
+    def run(self, connection, query, data, host, user, passwd, db, key):
         if connection:
             self.db = self.config_conn(connection=connection)
         else:
@@ -14,9 +16,9 @@ class MySQLSelectAction(MySQLBaseAction):
                                        passwd=passwd,
                                        db=db)
 
-        return self.select(query, data)
+        return self.select(query, data, key)
 
-    def select(self, query, data):
+    def select(self, query, data, key):
         q = query
         if data:
             values = self._list_to_string(data)
@@ -27,7 +29,12 @@ class MySQLSelectAction(MySQLBaseAction):
         try:
             c.execute(q)
             output = self._format_results(c)
-            return output
+            if key != None:
+                client = Client(base_url='http://localhost')
+                client.keys.update(KeyValuePair(name=key, value=str(output)))
+                return key
+            else:
+                return output
         except MySQLdb.Error, e:
             print str(e)
             return False
