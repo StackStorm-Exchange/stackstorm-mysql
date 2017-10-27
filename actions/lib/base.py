e0903fe5b7b196ced0cf2b5c3948c85016280441
Kirill Izotov <enykeev@stackstorm.com>
2017-10-27 14:55:05 +0700

    Remove deprecated st2actions import

--- actions/lib/base.py
+++ actions/lib/base.py
@@ -2,7 +2,7 @@ import MySQLdb
 import MySQLdb.cursors
 from datetime import datetime
 
-from st2actions.runners.pythonrunner import Action
+from st2common.runners.base_action import Action
 
 __all__ = [
     'MySQLBaseAction'
