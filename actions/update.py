a6e5b26e84638fd947a6baab33529e62bd8ddb0f
Alexandre Juma <alexandre.juma@gmail.com>
2018-06-23 18:21:30 +0100

    Added MySQL UPDATE action

---
+++ actions/update.py
@@ -0,0 +1,34 @@
+import MySQLdb
+
+from lib.base import MySQLBaseAction
+from st2client.client import Client
+from st2client.models import KeyValuePair
+
+
+class MySQLSelectAction(MySQLBaseAction):
+
+    def run(self, connection, query, data, host, user, passwd, db):
+        if connection:
+            self.db = self.config_conn(connection=connection)
+        else:
+            self.db = self.manual_conn(host=host,
+                                       user=user,
+                                       passwd=passwd,
+                                       db=db)
+
+        return self.update(query, data)
+
+    def update(self, query, data):
+        q = query
+        if data:
+            values = self._list_to_string(data)
+            q = q.format(values)
+
+        c = self.db.cursor()
+        try:
+            c.execute(q)
+            count = c.rowcount
+            self.db.commit()
+            return count
+        except MySQLdb.Error, e:  # pylint: disable=no-member
+            raise Exception(e)
